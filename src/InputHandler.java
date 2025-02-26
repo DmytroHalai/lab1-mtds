ba48063c05d4e1d873133bfb25f56463505fa92a
DmytroHalai <dimagalai04@gmail.com>
2025-02-26 23:56:04 +0200

    Add methods in input handler
    Add method to get valid double, which gets two arguments: console and variable name, it checks id user entered valid input and if not, it restarts the input process for the given variable

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -1,2 +1,23 @@
-public class InputHandler {
+import java.util.Scanner;
+
+class InputHandler {
+    public static double[] getCoefficients(Scanner scanner) {
+        double[] coefficients = new double[3];
+        for (int i = 0; i < 3; i++) {
+            coefficients[i] = getValidDouble(scanner, "Coefficient " + (char) ('a' + i));
+        }
+        return coefficients;
+    }
+
+    private static double getValidDouble(Scanner scanner, String varName) {
+        while (true) {
+            System.out.print(varName + " = ");
+            String input = scanner.next();
+            try {
+                return Double.parseDouble(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Error. Expected a valid real number, got \"" + input + "\" instead.");
+            }
+        }
+    }
 }
