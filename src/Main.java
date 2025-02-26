c9e0d388c2bff7c33bfdafe8ae24c5d1aa7e2c30
DmytroHalai <dimagalai04@gmail.com>
2025-02-27 00:19:44 +0200

    Add initial implementation in Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] coefficients;
+
+        if (args.length > 0) {
+            try {
+                coefficients = InputHandler.getCoefficientsFromFile(args[0]);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        } else {
+            coefficients = InputHandler.getCoefficients(scanner);
+        }
+
+        EquationSolver solver = new EquationSolver(coefficients[0], coefficients[1], coefficients[2]);
+        solver.solve();
     }
 }
