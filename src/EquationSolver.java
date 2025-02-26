5c4ce4845aadb57c442888abcb2f019abe4c4a7e
DmytroHalai <dimagalai04@gmail.com>
2025-02-27 00:48:10 +0200

    Add optimization for methods in EquationSolver

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -9,21 +9,34 @@ class EquationSolver {
         this.c = c;
     }
 
-    public void solve() {
+    private void printEquation(){
         System.out.println("The equation is: (" + a + ") x^2 + (" + b + ") x + (" + c + ") = 0");
+    }
 
-        double disc = b * b - 4 * a * c;
+    public void solve() {
+        printEquation();
+
+        double disc = calcDisc();
         if (disc < 0) {
             System.out.println("There are 0 roots");
         } else if (disc == 0) {
             System.out.println("There is 1 root");
-            System.out.println("x1 = " + (-b) / (2 * a));
+            System.out.println("x1 = " + calcRoot(disc, true));
         } else {
-            double x1 = (-b - Math.sqrt(disc)) / (2 * a);
-            double x2 = (-b + Math.sqrt(disc)) / (2 * a);
+            double x1 = calcRoot(disc, true);
+            double x2 = calcRoot(disc, false);
             System.out.println("There are 2 roots");
             System.out.println("x1 = " + x1);
             System.out.println("x2 = " + x2);
         }
+    }
+
+    private double calcDisc(){
+        return b * b - 4 * a * c;
+    }
+
+    private double calcRoot(double disc, boolean isFirst){
+        double sqrtDisc = isFirst ? -1 * Math.sqrt(disc) : Math.sqrt(disc);
+        return (-b + sqrtDisc) / (2 * a);
     }
 }
